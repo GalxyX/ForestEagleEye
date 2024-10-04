80737255bd39b6d2549c22c74b81d1ab787358f1
宇轩 蔡 <1812003940@qq.com>
2024-10-04 20:43:21 +0800

    完成login和register前端

---
+++ ForestEagleEye/src/views/RegisterView.vue
@@ -0,0 +1,147 @@
+<template>
+    <div class="login-container" >
+      <div class="login-box" style="overflow: hidden;">
+        <div class="form-container">
+          <h2 style="text-align: center; font-weight: bold;">Create an Account</h2>
+          <h5 style="text-align: center; color: grey; font-weight: 100;">注册成为林上鹰眼的一员</h5>
+          <form @submit.prevent="handleLogin">
+            <div class="form-group">
+              <label for="username">Username</label>
+              <input type="username" id="username" v-model="username" placeholder="请输入用户名" required />
+            </div>
+            <div class="form-group">
+              <label for="email">Email</label>
+              <input type="email" id="email" v-model="email" placeholder="请输入账号的电子邮箱" required />
+            </div>
+            <div class="form-group">
+              <label for="password">Password</label>
+              <input type="password" id="password" v-model="password" placeholder="请输入账号的密码" required />
+            </div>
+            <button type="submit">Get Started</button>
+            <p>已有账号？ <a href="/login">Sign in</a></p>
+          </form>
+        </div>
+        <div class="image-container">
+          <img src="@/assets/login.png" alt="" />
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import { ref } from 'vue';
+  
+  export default {
+    setup() {
+      const email = ref('');
+      const password = ref('');
+  
+      const handleLogin = () => {
+        // 登录逻辑
+        console.log('Logging in with:', email.value, password.value);
+      };
+  
+  
+      return {
+        email,
+        password,
+        handleLogin,
+      };
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .login-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: #ddd;
+  }
+  
+  .login-box {
+    display: flex;
+    background: white;
+    border-radius: 30px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    width: 100%;
+    max-width: 1000px;
+    align-items: center;
+    margin-left: 10px;
+    margin-right: 10px;
+  }
+  
+  .image-container img {
+    display: block;
+    max-height: 70vh;
+  }
+  
+  .form-container {
+    flex: 1;
+    padding: 10px;
+  }
+  
+  .form-group {
+    margin-bottom: 25px;
+    margin-left: 10px;
+    margin-right: 10px;
+  }
+  
+  .form-group label {
+    display: block;
+    margin-bottom: 5px;
+    font-size: smaller;
+  }
+  
+  .form-group input {
+    width: 90%;
+    padding: 10px;
+    border: 1.5px solid #ddd;
+    border-radius: 10px;
+  }
+  .form-group input:hover {
+    border: 1.5px solid #60A130;
+  }
+  .form-group input:focus {
+    border: 1.5px solid #60A130;
+    background-color: #f6fdf3;
+    outline: none;
+  }
+  
+  button {
+    width: 90%;
+    padding: 10px;
+    border:  1.5px solid black;
+    border-radius: 10px;
+    background-color: white;
+    color: black;
+    cursor: pointer;
+    align-content: center;
+    margin-left: 10px;
+    margin-right: 10px;
+    font-weight: bold;
+  }
+  
+  button:hover {
+    background-color: black;
+    color: white;
+  }
+  
+  p {
+    margin-left: 10px;
+    margin-right: 10px;
+    font-size: smaller;
+    text-align: center;
+    color: gray;
+  }
+  
+  a {
+    color: #60A130;
+    text-decoration: none;
+  }
+  
+  a:hover {
+    text-decoration: underline;
+  }
+  </style>
