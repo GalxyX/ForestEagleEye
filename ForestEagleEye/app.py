ffc73c0a402d5fc6159690bd947cd91be6a5952c
vivi <2251927@tongji.edu.cn>
2024-10-05 11:59:21 +0800

    审批流

--- ForestEagleEye/app.py
+++ ForestEagleEye/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect, session, url_for, f
 import pymysql
 import hashlib
 import sqlalchemy
+from sqlalchemy import insert
+from werkzeug.utils import secure_filename
 from sqlalchemy import create_engine, Column, Enum,Integer,Table, String, ForeignKey, DateTime,Text,Boolean
 from sqlalchemy.orm import sessionmaker, relationship, declarative_base
 from datetime import datetime
@@ -9,11 +11,17 @@ from flask import render_template #引入模板插件
 from sqlalchemy.testing import db
 from flask_mail import Mail, Message
 import random
+import os
+
+
 
 app = Flask(__name__,
 static_folder='./public/static',  #设置静态文件夹目录
 template_folder = "./public/templates")  #设置vue编译输出目录dist文件夹，为Flask模板文件目录
 app.secret_key = '123456789'
+
+UPLOAD_FOLDER = os.path.join(app.static_folder, 'uploads')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 app.config['MAIL_SERVER'] = 'smtp.qq.com'  # 邮件服务器
 app.config['MAIL_PORT'] = 587
@@ -28,22 +36,24 @@ verification_codes = {}  # 存储邮箱和验证码的字典
 # MySQL 数据库连接配置
 db_config={
     'user':'root',
-    'password':'hxyym123',#这里改成自己的数据库密码
+    'password':'',#这里改成自己的数据库密码
     'host':'localhost',
     'port':3306,
-    'database': 'forest',#这里改成自己的数据库名字
+    'database': '',#这里改成自己的数据库名字
     'charset':'utf8mb4'}
 # 创建数据库连接
 engine = create_engine('mysql+pymysql://{user}:{password}@{host}:{port}/{database}?charset={charset}'.format(**db_config))
 
 Base = declarative_base()
 
-user_participate_activity = Table(
-    "user_participate_activity",
-    Base.metadata,
-    Column("user_id", Integer, ForeignKey("users.u_id")),
-    Column("activity_id", Integer, ForeignKey("activities.a_id")),
-)
+
+class user_participate_activity(Base):
+    __tablename__ = 'user_participate_activity'
+    upa_id = Column(Integer, primary_key=True, nullable=False, unique=True)  # 唯一标识
+    participateNumber = Column(Integer, nullable=False)  # 此次报名活动人数
+    note = Column(String(100), default="")  # 备注
+    user_id = Column(Integer, ForeignKey('users.u_id'))
+    activity_id = Column(Integer, ForeignKey('activities.a_id'))
 
 
 # 用户表
@@ -61,10 +71,6 @@ class User(Base):
     u_signupTime = Column(DateTime, nullable=False, default=datetime.now)  # 注册时间
     u_newestTime = Column(DateTime, nullable=False, onupdate=datetime.now, default=datetime.now)  # 最近登录时间
 
-    # 联系集
-    u_participateActivity = relationship("Activity", secondary=user_participate_activity, back_populates="a_participants")  # 报名参加的活动
-    u_applyActivities = relationship("Activity", backref="applicant")  # 申请的活动
-    u_approveActivity = relationship("ApprovalProcess", backref="approver")  # 审批的活动
 
     #u_submitTips = relationship()  # 提交的建议与举报
     #u_approveTips = relationship()  # 审批的建议与举报
@@ -82,7 +88,7 @@ class Activity(Base):
     a_submitTime = Column(DateTime, nullable=False, default=datetime.now)  # 申请提交时间
     a_attachment = Column(String(100), default="")  # 申请附件
     a_name = Column(String(100), nullable=False)  # 活动名称
-    # a_type = Column(Enum("", "", "", "", ""), nullable=False)  # 活动类型
+    a_type = Column(Enum("伐木", "采摘", "旅游参观", "野营", "捕猎"), nullable=False)  # 活动类型
     a_forest = Column(String(100), nullable=False)  # 审批单位（森林名称）
     a_location = Column(String(100), nullable=False)  # 活动地点（具体地点）
     a_beginTime = Column(DateTime, nullable=False)  # 活动开始时间
@@ -91,24 +97,19 @@ class Activity(Base):
     a_picPath = Column(String(100), default="")  # 活动图片路径
     a_ableParticipate = Column(Boolean, nullable=False)  # 活动是否面向大众（大众可以报名参加）
     a_participantNumber = Column(Integer, nullable=False)  # 活动人数
+    a_enrolledNumber = Column(Integer, nullable=False, default=0)  # 已报名人数
     a_state = Column(Enum("approving", "approved", "dismissed", "ongoing", "ended"), nullable=False, default="approving")  # 状态（待审批、通过、驳回、活动开始、活动完成）
+    a_approver_id = Column(Integer, ForeignKey('users.u_id'),nullable=True) #审批人id
+    a_approveTime = Column(DateTime, nullable=True)  # 活动审批时间
+    a_dismissreason = Column(String(100), nullable=True) #驳回理由
 
-    # 联系集
-    a_participants = relationship("User", secondary=user_participate_activity, back_populates="u_participateActivity")  # 报名参加的人
-    a_approvalProcessId = relationship("ApprovalProcess", backref="activities")  # 审批的所有结点编号
 
 
-# 审批流结点
-class ApprovalProcess(Base):
-    __tablename__ = "approvalprocesses"
-    p_id = Column(Integer, primary_key=True, nullable=False, unique=True)
-    p_lastId = Column(Integer, nullable=False)  # 上一个审批结点  ## null表示为第一个审批结点
-    p_activityId = Column(Integer, ForeignKey("activities.a_id"), nullable=False)  # 本结点对应的活动
-    p_approverId = Column(Integer, ForeignKey("users.u_id"), nullable=False)  # 本结点上的审批者
-    a_submitTime = Column(DateTime, nullable=False, default=datetime.now)  # 本次审批提交时间
-    a_approveTime = Column(DateTime)  # 本次审批审批时间
-    p_result = Column(Enum("approved", "dismissed"), nullable=False)  # 审批意见（通过、驳回）
-    p_notes = Column(String(1000))  # 审批理由
+
+
+# 这是删除所有数据的操作，不到万不得已千万不要做
+# 如果之前创建过同名数据库且不明白如何数据库迁移，可以把下面一句注释去掉，运行清除之前的表并创建新表，然后记得加上注释
+#Base.metadata.drop_all(engine)
 
 Base.metadata.create_all(engine)
 
@@ -181,11 +182,288 @@ def login():
             return render_template('login.html', error=error)
     return render_template('login.html')
 
-@app.route('/logout')
-def logout():
+#注销
+@app.route('/logoff')
+def logoff():
     session.clear()
     flash('您已成功注销', 'success')
     return redirect(url_for('login'))
+
+#退出登录
+@app.route('/logout')
+def logout():
+    return redirect(url_for('login'))
+
+#林业活动界面
+@app.route('/activity')
+def activity():
+    username = session.get('username')  # 从会话中获取用户名
+    if not username:
+        return redirect(url_for('login'))  # 如果没有用户名信息，重定向到登录页面
+
+    # 根据用户名查询用户角色
+    user = db_session.query(User).filter_by(u_name=username).first()
+    if user:
+        u_role = user.u_role
+    else:
+        flash('用户不存在或已被删除', 'error')
+        return redirect(url_for('login'))
+    #传递当前用户角色给前端，根据不同角色显示不同的按钮
+    return render_template('activity.html', u_role=u_role)
+
+#我的申请界面，显示当前用户已申请的活动
+@app.route('/apply')
+def apply():
+    if 'username' in session:
+        # 获取当前用户的申请中或已通过的活动
+        username = session.get('username')  # 从会话中获取用户名
+        user = db_session.query(User).filter_by(u_name=username).first()
+        if user:
+            u_id = user.u_id
+        approving_activities = db_session.query(Activity).filter(Activity.a_applicantId == u_id,
+                                                                 Activity.a_state == 'approving').all()
+        approved_activities = db_session.query(Activity).filter(Activity.a_applicantId == u_id,
+                                                                Activity.a_state == 'approved').all()
+        dismissed_activities = db_session.query(Activity).filter(Activity.a_applicantId == u_id,
+                                                                 Activity.a_state == 'dismissed').all()
+
+        return render_template('apply.html',approving_activities=approving_activities, approved_activities=approved_activities, dismissed_activities=dismissed_activities)
+    return redirect(url_for('index'))
+
+
+
+#创建活动
+@app.route('/create_activity', methods=['GET', 'POST'])
+def create_activity():
+
+    if request.method == 'POST':
+        # 从表单中获取数据
+        a_name = request.form['a_name']
+        a_location = request.form['a_location']
+        a_beginTime = request.form['a_beginTime']
+        a_endTime = request.form['a_endTime']
+        a_participantNumber = request.form['a_participantNumber']
+        a_introduction = request.form['a_introduction']
+        a_forest =request.form['a_forest']
+        a_type = request.form['a_type']
+        a_ableParticipate = request.form.get('a_ableParticipate') is not None
+
+        # 处理文件上传
+        a_picPath = request.files['a_picPath']
+        if a_picPath:
+            filename = secure_filename(a_picPath.filename)
+            a_picPath.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        # 根据用户名查询用户
+        username = session.get('username')  # 从会话中获取用户名
+        user = db_session.query(User).filter_by(u_name=username).first()
+        if user:
+            u_id = user.u_id
+        # 创建新的 Activity 实例
+        new_activity = Activity(
+            a_applicantId=u_id,
+            a_name=a_name,
+            a_location=a_location,
+            a_beginTime=datetime.strptime(a_beginTime, '%Y-%m-%dT%H:%M'),
+            a_endTime=datetime.strptime(a_endTime, '%Y-%m-%dT%H:%M'),
+            a_participantNumber=int(a_participantNumber),
+            a_introduction=a_introduction,
+            a_picPath=filename,
+            a_ableParticipate=a_ableParticipate,
+            a_type = a_type,
+            a_forest=a_forest,
+            a_state="approving"  # 默认状态为待审批
+        )
+
+        # 将新活动添加到数据库会话并提交
+        db_session.add(new_activity)
+        db_session.commit()
+
+        flash('活动创建成功，等待审批', 'success')
+        return redirect(url_for('activity'))
+
+    return render_template('create_activity.html')
+
+#活动详情页，申请人和审批人公用界面，但是会传递一个is_approver参数决定前端是否显示“同意”“驳回”
+@app.route('/activity_detail/<int:activity_id>')
+def activity_detail(activity_id):
+    activity = db_session.query(Activity).filter_by(a_id=activity_id).first()
+    if not activity:
+        flash('活动不存在', 'error')
+        return redirect(url_for('activity'))
+
+    username = session.get('username')  # 从会话中获取用户名
+    user = db_session.query(User).filter_by(u_name=username).first()
+    current_user_role = user.u_role
+    # 判断当前用户是否为审批人
+    is_approver = current_user_role == '林业管理人员'
+    return render_template('activity_detail.html', activity=activity,is_approver=is_approver)
+
+
+#申请人删除所申请的活动
+@app.route('/delete_activity/<int:activity_id>', methods=['POST'])
+def delete_activity(activity_id):
+
+    activity = db_session.query(Activity).filter_by(a_id=activity_id).first()
+    if activity:
+        db_session.delete(activity)
+        db_session.commit()
+        flash('活动删除成功', 'success')
+    else:
+        flash('活动不存在或已被删除', 'error')
+
+    return redirect(url_for('apply'))
+
+#我的审批界面
+@app.route('/approve')
+def approve():
+    # 假设当前管理员的森林名称存储在session中
+    username = session.get('username')  # 从会话中获取用户名
+    user = db_session.query(User).filter_by(u_name=username).first()
+    current_forest = user.u_forest
+
+    if not current_forest:
+        flash('您尚未登录或没有权限访问此页面', 'error')
+        return redirect(url_for('login'))
+
+    # 根据当前管理员的森林名称和状态查询活动
+    approving_activities = db_session.query(Activity).filter(Activity.a_forest == current_forest,
+                                                             Activity.a_state == 'approving').all()
+    approved_activities = db_session.query(Activity).filter(Activity.a_forest == current_forest,
+                                                            Activity.a_state == 'approved').all()
+    dismissed_activities = db_session.query(Activity).filter(Activity.a_forest == current_forest,
+                                                             Activity.a_state == 'dismissed').all()
+
+    # 渲染模板并传递活动列表,传递用户的角色是因为在活动详情页时如果为审批人会添加审批通过或驳回按钮
+    return render_template('approve.html', approving_activities=approving_activities,
+                           approved_activities=approved_activities, dismissed_activities=dismissed_activities)
+
+
+#审批通过
+@app.route('/approve_activity/<int:activity_id>', methods=['POST'])
+def approve_activity(activity_id):
+    # 审批活动逻辑
+    # 更新活动状态为 'approved'
+    activity = db_session.query(Activity).filter_by(a_id=activity_id).first()
+    #获取当前审批人的id
+    username = session.get('username')  # 从会话中获取用户名
+    user = db_session.query(User).filter_by(u_name=username).first()
+    #修改状态
+    activity.a_state= 'approved'
+    #存储审批人的id
+    activity.a_approver_id = user.u_id
+    #存储审批时间
+    activity.a_approveTime = datetime.now()
+
+    return redirect(url_for('approve'))
+
+
+#审批驳回
+@app.route('/dismiss_activity/<int:activity_id>', methods=['POST'])
+def dismiss_activity(activity_id):
+    dismiss_reason = request.form.get('dismiss_reason', '')
+    activity = db_session.query(Activity).filter_by(a_id=activity_id).first()
+    #获取当前审批人的id
+    username = session.get('username')  # 从会话中获取用户名
+    user = db_session.query(User).filter_by(u_name=username).first()
+    #修改状态
+    activity.a_state = 'dismissed'
+    #存储审批人的id
+    activity.a_approver_id = user.u_id
+    #存储审批时间
+    activity.a_approveTime = datetime.now()
+    #存储驳回理由
+    activity.a_dismissreason = dismiss_reason
+    return redirect(url_for('approve'))
+
+# 活动风采界面
+@app.route('/activities')
+def activities():
+    #筛选面向大众且截止时间晚于当前时间且审批通过的活动
+    # 获取当前时间
+    now = datetime.now()
+    activities = db_session.query(Activity).filter(Activity.a_ableParticipate == True, Activity.a_endTime > now,Activity.a_state=='approved').all()
+    return render_template('activities.html', activities=activities)
+
+#报名活动界面
+@app.route('/activity_enroll/<int:activity_id>',methods=['GET'])
+def activity_enroll(activity_id):
+    activity = db_session.query(Activity).filter_by(a_id=activity_id).first()
+    if activity:
+        return render_template('activity_enroll.html', activity=activity)
+    else:
+        flash('活动不存在或已结束', 'error')
+        return redirect(url_for('activities'))
+
+
+#点击报名后
+@app.route('/enroll/<int:activity_id>', methods=['POST'])
+def enroll(activity_id):
+    activity = db_session.query(Activity).filter_by(a_id=activity_id).first()
+    if not activity or not activity.a_ableParticipate or activity.a_endTime <= datetime.now():
+        flash('活动不可报名或已结束', 'error')
+        return redirect(url_for('activities'))
+
+    participant_number = request.form['participantNumber']
+    remark = request.form.get('remark', '')  # 获取备注信息，默认为空字符串
+    if int(participant_number) > (activity.a_participantNumber - activity.a_enrolledNumber):
+        flash('报名人数超过剩余名额', 'error')
+        return redirect(url_for('activity_enroll', activity_id=activity_id))
+
+    # 获取用户id
+    username = session.get('username')  # 从会话中获取用户名
+    user_id = db_session.query(User).filter_by(u_name=username).first().u_id
+    # 创建用户和活动的联系
+    activity.a_enrolledNumber += int(participant_number)
+    new_participant = user_participate_activity(
+        participateNumber =  participant_number,
+        note =  remark,
+        user_id=user_id,
+        activity_id=activity_id,
+    )
+    db_session.add(new_participant)
+    db_session.commit()
+    flash('报名成功', 'success')
+    return redirect(url_for('activities'))
+
+
+#我的报名界面
+@app.route('/myenrolled')
+def myenrolled():
+    username = session.get('username')  # 从会话中获取用户名
+    user = db_session.query(User).filter_by(u_name=username).first()
+    if user:
+        woos= db_session.query(user_participate_activity).filter_by(user_id=user.u_id).all()
+        # 从每个记录中提取 activity_id，并将其存储在列表中
+        activity_ids = [woo.activity_id for woo in woos]
+    else:
+        flash('找不到用户', 'error')
+        return redirect(url_for('login'))
+    participations= db_session.query(Activity).filter(Activity.a_id.in_(activity_ids)).all()
+
+    return render_template('myenrolled.html', participations=participations)
+
+#普通用户取消报名
+@app.route('/cancel_enrollment/<int:activity_id>', methods=['POST'])
+def cancel_enrollment(activity_id):
+    username = session.get('username')  # 从会话中获取用户名
+    user = db_session.query(User).filter_by(u_name=username).first()
+    if not user:
+        flash('您尚未登录或没有权限执行此操作', 'error')
+        return redirect(url_for('login'))
+
+    # 查询
+    participation = db_session.query(user_participate_activity).filter_by(user_id=user.u_id, activity_id=activity_id).first()
+    activity = db_session.query(Activity).filter_by(a_id=activity_id).first()
+    activity.a_enrolledNumber -= participation.participateNumber
+    if participation:
+        db_session.delete(participation)
+        db_session.commit()
+        flash('取消报名成功', 'success')
+    else:
+        flash('取消报名失败，可能您已取消报名或不存在此活动', 'error')
+
+    return redirect(url_for('myenrolled'))
 
 @app.route('/')
 def index():
